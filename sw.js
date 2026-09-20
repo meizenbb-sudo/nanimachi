ca7b6498cdc1316ca18baf309c5da949504cff76
meizenbb-sudo <meizenbb@gmail.com>
2026-09-21 01:03:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * 本体が変わるとビルドが下の VERSION を書き換えるので、新しい版が入ったら画面に「更新」の帯を出す。
  * 書体（Google Fonts）は使ったときに保存する。取れなくても端末の丸ゴシックで代替できるので必須にはしない。
  */
-const VERSION = '0365c019f4db';
+const VERSION = '9cad1314ca9c';
 const CACHE = `haiyomi-${VERSION}`;
 const FONTS = 'haiyomi-fonts';
 const SHELL = new URL('./', self.registration.scope).href;
